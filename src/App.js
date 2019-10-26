3214b1b93688733b3d86b1377ede17ce8a61678d
Samuel <samuelnnj@gmail.com>
2019-10-26 18:38:57 +0100

    broke hompage into different components and styled them

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Homepage from './Hompage.component'
+import Homepage from './pages/homepage/homepage.component'
 
 import './App.css';
 
---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './homepage.style.scss';
+
+import Directory from '../../component/directory/directory.component';
+
+const Homepage = () => (
+    <div className='homepage'>
+        <Directory />
+    </div>
+)
+
+export default Homepage;

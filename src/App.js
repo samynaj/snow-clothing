30c83823907bb7ab07d25d597fd8ceb6d7b36d9e
Samuel <samuelnnj@gmail.com>
2019-11-07 14:36:07 +0100

    added sign in component with google authentication using firebase libarary

--- src/App.js
+++ src/App.js
@@ -5,19 +5,45 @@ import {Switch, Route} from 'react-router-dom';
 import './App.css';
 import ShopPage from './pages/shop/shop.component';
 import Header from './component/header/header.component';
-import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up'
+import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up';
+import { auth } from '../src/firebase/firebase.util'
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path='/' component={Homepage} />
-        <Route path='/shop' component={ShopPage} />
-        <Route path='/signin' component={ SignInAndSignUp } />
-      </Switch>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state= {
+      currentUser: null
+    }
+  }
+
+  unSubscribeFromAuth= null;
+
+  componentDidMount() {
+    this.unSubscribeFromAuth= auth.onAuthStateChanged(user => {
+      this.setState({currentUser: user});
+
+      console.log(user)
+    })
+  }
+
+  componentWillUnmount(){
+    this.unSubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div>
+        <Header currentUser={this.state.currentUser} />
+        <Switch>
+          <Route exact path='/' component={Homepage} />
+          <Route path='/shop' component={ShopPage} />
+          <Route path='/signin' component={ SignInAndSignUp } />
+        </Switch>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
